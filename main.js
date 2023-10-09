3aef1738e75ed3abd1ca3b23d6f95f67a4811f6c
Pongsapat_1022 <pongsapat.inta@kmutt.acth>
2023-10-10 00:49:40 +0700

    Add:Back Buttn Project To Port Add : Project in Port

--- main.js
+++ main.js
@@ -10,22 +10,30 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+document.addEventListener("DOMContentLoaded", function() {
+    const backButton = document.getElementById("backButton");
+
+    backButton.addEventListener("click", function(event) {
+        event.preventDefault();
+        goToBackPage();
+    });
+});
 function goToRecipePage() {
-    window.open("http://127.0.0.1:5500/Recipe/Recipe.html", "_blank");  }
+    window.open("http://127.0.0.1:5501/Recipe/Recipe.html", "_blank");  }
     
 
 function goToGridRecipePage() {
-    window.open("http://127.0.0.1:5500/GridEdit/Grid.html", "_blank");  }    
+    window.open("http://127.0.0.1:5501/GridEdit/Grid.html", "_blank");  }    
 
 function goToTextToPage(){
-    window.open("http://127.0.0.1:5500/assignment5/index.html", "_blank");  
+    window.open("http://127.0.0.1:5501/assignment5/index.html", "_blank");  
 }
 function goToRegisterPage() {
-    window.open("http://127.0.0.1:5500/assignment6/register.html", "_blank");  }
+    window.open("http://127.0.0.1:5501/assignment6/register.html", "_blank");  }
     
 
 function goToLoginPage() {
-    window.open("http://127.0.0.1:5500/assignment6/login.html", "_blank");  }    
+    window.open("http://127.0.0.1:5501/assignment6/login.html", "_blank");  }    
 
 function goToGameBoxage(){
     window.open("http://127.0.0.1:5501/assignment7/index.html", "_blank");  
@@ -33,4 +41,9 @@ function goToGameBoxage(){
 function goToSoulEscapePage(){
     window.open("https://play.google.com/store/apps/details?id=com.Pie.Game");
 }
-
+function goToProjectPage(){
+    window.location.replace("http://127.0.0.1:5501/Project.html");
+}
+function goToBackPage() {
+    window.location.href = "http://127.0.0.1:5501/PortFolio.html";
+}
