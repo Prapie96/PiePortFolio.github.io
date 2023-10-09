ede79b7994a56028fafb9695eeec61d192b179be
Pongsapat_1022 <pongsapat.inta@kmutt.acth>
2023-10-09 20:08:39 +0700

    Add:PortFolioFiles

---
+++ main.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const nav = document.querySelector("nav");
+    const navOffset = nav.offsetTop;
+
+    window.addEventListener("scroll", function() {
+        if (window.pageYOffset >= navOffset) {
+            nav.classList.add("fixed");
+        } else {
+            nav.classList.remove("fixed");
+        }
+    });
+});
+function goToRecipePage() {
+    window.open("http://127.0.0.1:5500/Recipe/Recipe.html", "_blank");  }
+    
+
+function goToGridRecipePage() {
+    window.open("http://127.0.0.1:5500/GridEdit/Grid.html", "_blank");  }    
+
+function goToTextToPage(){
+    window.open("http://127.0.0.1:5500/assignment5/index.html", "_blank");  
+}
+function goToRegisterPage() {
+    window.open("http://127.0.0.1:5500/assignment6/register.html", "_blank");  }
+    
+
+function goToLoginPage() {
+    window.open("http://127.0.0.1:5500/assignment6/login.html", "_blank");  }    
+
+function goToTextToPage(){
+    window.open("http://127.0.0.1:5500/assignment5/index.html", "_blank");  
+}
+
+
